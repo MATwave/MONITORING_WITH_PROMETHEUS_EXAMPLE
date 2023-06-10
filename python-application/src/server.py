bc9f470cc7cfa112ab2a0146bbe7edfffaa9f899
stanislav <stanislav.matveew@yandex.ru>
2023-06-10 18:42:05 +0300

    refactor: немного переделал код

--- python-application/src/server.py
+++ python-application/src/server.py
@@ -1,33 +1,29 @@
 import time
 
-import prometheus_client
 from flask import Response, Flask
-from prometheus_client import Counter, Histogram
+from prometheus_client import Counter, Histogram, generate_latest
 
 app = Flask(__name__)
 
-_INF = float("inf")
-
-graphs = {}
-graphs['c'] = Counter('python_request_operations_total', 'The total number of processed requests')
-graphs['h'] = Histogram('python_request_duration_seconds', 'Histogram for the duration in seconds.',
-                        buckets=(1, 2, 5, 6, 10, _INF))
+graphs = {
+    'REQUEST_COUNT': Counter('python_request_operations_total',
+                             'The total number of processed requests'),
+    'REQUEST_LATENCY': Histogram('python_request_duration_seconds',
+                                 'Histogram for the duration in seconds.',
+                                 buckets=(1, 2, 5, 6, 10, float("inf")))
+}
 
 
 @app.route("/")
 def hello():
-    start = time.time()
-    graphs['c'].inc()
+    graphs['REQUEST_COUNT'].inc()
 
-    time.sleep(0.600)
-    end = time.time()
-    graphs['h'].observe(end - start)
-    return "Hello World!"
+    with graphs['REQUEST_LATENCY'].time():
+        time.sleep(0.600)
+        return "Hello World!"
 
 
 @app.route("/metrics")
 def requests_count():
-    res = []
-    for k, v in graphs.items():
-        res.append(prometheus_client.generate_latest(v))
+    res = [generate_latest(v) for v in graphs.values()]
     return Response(res, mimetype="text/plain")
